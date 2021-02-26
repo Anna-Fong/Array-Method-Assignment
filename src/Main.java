2dc5382a215f6d2d9b981c74421f6e642ecd4723
Anna Fong <757172@learn.vsb.bc.ca>
2021-02-26 14:58:48 -0800

    3

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,57 @@
 
 public class Main {
 
-    public static int oldArray(int number) {
 
         //loop through array
         //loop old array to new array
         //add on the number to last position
         //return new array
 
-        int a = number;
+        //	• Add values to the end of an array.
+        //	• Delete values from the end of an array (often referred to as pop).
+        //	This method should also return the 'popped' value. Array size should shrink by one.
+        //Insert values into an array at chosen index position.
+        // (Increases the size of the array by 1 Ex. array 2, 3, 5.  Insert 4 into position 2 gives 2,3,4,5)
+
+    public static int oldArray(int length) {
+
+        int oldArrayLength = length;
+        int base = 0;
+        int a = length;
 
         System.out.println("Index: \tValue:");
-        System.out.println("--------------");
 
-        for (int i = 0; i < number; i++) {
-            int rand = (int) ((Math.random() * 20) + 1);
-            if (i == number) {
+        for (int i = 0; i < length; i++) {
+            int value = (base += 2);
+            if (i == length) {
                 break;
             }
 
-            System.out.println(i + "\t\t" + rand);
+            System.out.println(i + "\t\t" + value);
         }
-        System.out.println("--------------");
         return a;
+    }
 
+    public static int newArray(int length) {
+
+        int newArrayLength = length;
+        int base = 0;
+        int a = length;
+
+        for (int i = 0; i < length; i++) {
+            int value = (base += 10);
+            if (i == length) {
+                break;
+            }
+
+            System.out.println(i + "\t\t" + value);
+        }
+        return a;
     }
 
     public static void main(String[] args) {
         System.out.println(oldArray(5));
-
+        System.out.println(newArray(2));
     }
 
 }
